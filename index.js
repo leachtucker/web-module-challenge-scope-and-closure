f39ee11a46ffec716c80fd36c368cec89428c9b5
Tucker Leach <leachtucker@gmail.com>
2020-08-17 12:13:09 -0700

    Finished task 1

--- index.js
+++ index.js
@@ -13,9 +13,9 @@
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+    return callback(stringList[0])
 }
 
 // ⭐️ Example Challenge END ⭐️
@@ -27,19 +27,19 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
- * 
+ *      counter1 uses a closure to continue updating the count variable's value. counter2, however, places count in the global scope to retain the variables value. 
  * 2. Which of the two uses a closure? How can you tell?
- * 
+ *      counter1 invokes the counterMaker function which has a function nested inside of it. Nesting a function inside of another funciton is called a closure. 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
-*/
+ *      counter1 would be used when you want to keep the count variable private and out of the global scope. For example, you could use it when you have two similar functions  and you would like to use the count variable in each function without them interfering with each other. 
+ */
 
 // counter1 code
 function counterMaker() {
-  let count = 0;
-  return function counter() {
-   return count++;
-  }
+    let count = 0;
+    return function counter() {
+        return count++;
+    }
 }
 
 const counter1 = counterMaker();
@@ -48,7 +48,7 @@ const counter1 = counterMaker();
 let count = 0;
 
 function counter2() {
-  return count++;
+    return count++;
 }
 
 
@@ -56,7 +56,7 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning( /*Code Here*/ ) {
 
     /*Code Here*/
 
@@ -74,11 +74,11 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
+function finalScore( /*code Here*/ ) {
 
-  /*Code Here*/
+    /*Code Here*/
 
 }
 
@@ -103,8 +103,6 @@ and returns the score at each pont in the game, like so:
 Final Score: awayTeam - homeTeam */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard( /* CODE HERE */ ) {
+    /* CODE HERE */
 }
-
-
